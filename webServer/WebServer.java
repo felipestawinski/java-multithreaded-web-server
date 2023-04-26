0d2e7736d230b84e04fd3475bb13c881c70485a5
felipestawinski <felipe.stawinski@gmail.com>
2023-04-25 21:42:41 -0300

    webServer

---
+++ webServer/WebServer.java
@@ -0,0 +1,30 @@
+package webServer;
+import java.io.* ;
+import java.net.* ;
+import java.util.* ;
+public final class WebServer
+{
+public static void main(String argv[]) throws Exception
+{
+    int port = 6789;
+
+    try {
+        ServerSocket serverSocket = new ServerSocket(port);
+        
+
+        while (true) {
+            
+            Socket socket = serverSocket.accept();
+            HttpRequest request = new HttpRequest(socket);
+            Thread thread = new Thread(request);
+            thread.start();
+        }
+
+    }
+    catch (Exception e) {
+        e.printStackTrace();
+    }
+    
+}
+}
+
